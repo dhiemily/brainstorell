7ac09eca930fc7f3ed62da98d85371c589851a5e
dhiemily <167090493+dhiemily@users.noreply.github.com>
2026-09-21 12:25:55 -0300

    js

--- scripty.js
+++ scripty.js
@@ -107,17 +107,22 @@ function mostraAlternativas(){
     }
 }
 
-function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmacao;
-    historiaFinal += afirmacoes + " ";
-    atual++;
-    mostraPergunta();
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+historiaFinal += afirmacoes + "  ";
+atual++;
+mostraPergunta();
 }
 
 function mostraResultado(){
     caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = ""; 
+}
+
+function aleatorio(lista) {
+const posicao = Math.floor(Math.random()* lista.length);
+return lista[posicao];
 }
 
 mostraPergunta();
